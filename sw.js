4b9141e1456a0558b6f782c135348fc727f0f245
skistudio2007-cloud <skistudio2007@gmail.com>
2026-09-06 21:29:39 +0530

    Fix PWABuilder manifest icons, relative paths and add service worker

---
+++ sw.js
@@ -0,0 +1,38 @@
+﻿// Simple offline-ready service worker for PWA
+const CACHE_NAME = 'water-sort-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './favicon.png',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
